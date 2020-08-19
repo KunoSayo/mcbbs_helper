8022d2a070be8df04cbe61dda81a931aafa60091
euOnmyoji <1418780411@qq.com>
2020-08-19 14:18:37 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,196 @@
+//https://doc.rust-lang.org/book/
+
+#![allow(unused_imports, dead_code)]
+
+use std::any::Any;
+use std::borrow::BorrowMut;
+use std::collections::{HashMap, LinkedList, VecDeque};
+use std::convert::TryFrom;
+use std::fs::File;
+use std::io::{BufReader, Bytes, Read, stdin};
+use std::rc::Rc;
+use std::time::SystemTime;
+
+use chrono::{DateTime, Local};
+use regex::Regex;
+use reqwest::header::COOKIE;
+use tokio::time::Duration;
+use webbrowser::Browser;
+
+const VANILLA: &str = "https://www.mcbbs.net/forum-qanda-1.html?mobile=no";
+const MU: &str = "https://www.mcbbs.net/forum-multiqanda-1.html?mobile=no";
+const MOD: &str = "https://www.mcbbs.net/forum-modqanda-1.html?mobile=no";
+const AROUND: &str = "https://www.mcbbs.net/forum-etcqanda-1.html?mobile=no";
+const VOID: &str = "https://www.mcbbs.net/forum-peqanda-1.html?mobile=no";
+
+static mut OPEN: bool = false;
+
+#[tokio::main]
+async fn get(url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let pattern: Regex = Regex::new(".*(?P<url>thread-[0-9]+-[0-9]+-[0-9]+\\.html).*class=\"s xst\">(?P<title>.*)\n*</a>\n*").unwrap();
+
+    let mut cookie_file = File::open("cookie.cookie").unwrap();
+    let mut cookie = String::new();
+    cookie_file.read_to_string(&mut cookie).unwrap();
+    let mut list = Vec::with_capacity(71);
+    loop {
+        if let Ok(resp) = reqwest::Client::new().get(url)
+            .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36")
+            .header("Cookie", &cookie)
+            .timeout(Duration::from_secs(5)).send().await {
+            let lines: Vec<String> = resp.text().await.map(|c| c.lines().map(|l| l.to_string()).collect()).unwrap();
+            let mut i = 0;
+            let mut found = false;
+            while i < lines.len() {
+                let line = &lines[i];
+
+                let matcher = pattern.captures(&*line);
+                if let Some(matcher) = matcher {
+                    for j in 0..6 {
+                        if lines[i + j].contains("金粒") {
+                            if !list.contains(line) {
+                                if !found {
+                                    println!();
+                                    found = true;
+                                }
+                                list.push(line.clone());
+                                while list.len() > 70 {
+                                    list.pop();
+                                }
+                                unsafe {
+                                    if OPEN {
+                                        if let Err(e) = webbrowser::open(&*format!("https://www.mcbbs.net/{}", &matcher["url"])) {
+                                            eprintln!("open failed {}", e);
+                                        }
+                                    }
+                                }
+                                let idx = lines[i + j].find(r#""xw1">"#).unwrap_or(0);
+                                println!("{} https://www.mcbbs.net/{} {} {}", DateTime::<Local>::from(SystemTime::now()).time().format("%H:%M:%S%.3f")
+                                    .to_string(), &matcher["url"], &matcher["title"], &lines[i + j][idx + 5..]);
+                            }
+                            break;
+                        }
+                    }
+                }
+
+
+                i += 1;
+            }
+        }
+        std::thread::sleep(Duration::from_secs_f32(5.0));
+    }
+}
+
+#[tokio::main]
+async fn water() -> Result<(), Box<dyn std::error::Error>> {
+    let pattern: Regex = Regex::new(r#".*viewthread.*tid=(?P<tid>\d+).*"s xst">(?P<title>.*)</a>.*"#).unwrap();
+    let mut list = Vec::with_capacity(71);
+    loop {
+        if let Ok(resp) = reqwest::Client::new().get("https://www.mcbbs.net/forum.php?mod=forumdisplay&fid=52&filter=author&orderby=dateline&mobile=no")
+            .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36")
+            .timeout(Duration::from_secs(5)).send().await {
+            if let Ok(content) = resp.text().await {
+                let lines: Vec<&str> = content.lines().collect();
+                for line in lines {
+                    let matcher = pattern.captures(&*line);
+                    if let Some(matcher) = matcher {
+                        let tid = &matcher["tid"];
+                        if tid.parse().unwrap_or(9000000) > 1000000 {
+                            if !list.contains(&tid.to_string()) {
+                                list.push(tid.to_string());
+                                while list.len() > 70 {
+                                    list.pop();
+                                }
+                                unsafe {
+                                    if OPEN {
+                                        if let Err(e) = webbrowser::open(&*format!("https://www.mcbbs.net/thread-{}-1-1.html", &matcher["tid"])) {
+                                            eprintln!("open failed {}", e);
+                                        }
+                                    }
+                                }
+                                println!("{} \"https://www.mcbbs.net/thread-{}-1-1.html {}", DateTime::<Local>::from(SystemTime::now()).time().format("%H:%M:%S%.3f")
+                                    .to_string(), &matcher["tid"], &matcher["title"]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        std::thread::sleep(Duration::from_secs_f32(5.0));
+    }
+}
+
+
+fn main() {
+    loop {
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("read input failed");
+        let input = input.trim();
+        match input {
+            "1" => {
+                println!("getting VANILLA question");
+                std::thread::spawn(|| {
+                    if let Err(err) = get(VANILLA) {
+                        eprintln!("{}", err);
+                    }
+                    println!("爬vanilla结束")
+                });
+            }
+            "2" => {
+                println!("getting MU question");
+                std::thread::spawn(|| {
+                    if let Err(err) = get(MU) {
+                        eprintln!("{}", err);
+                    }
+                    println!("爬MU结束")
+                });
+            }
+            "3" => {
+                println!("getting MOD question");
+                std::thread::spawn(|| {
+                    if let Err(err) = get(MOD) {
+                        eprintln!("{}", err);
+                    }
+                    println!("爬MOD结束")
+                });
+            }
+            "4" => {
+                println!("getting AROUND question");
+                std::thread::spawn(|| {
+                    if let Err(err) = get(AROUND) {
+                        eprintln!("{}", err);
+                    }
+                    println!("爬AROUND结束")
+                });
+            }
+            "5" => {
+                println!("getting VOID question");
+                std::thread::spawn(|| {
+                    if let Err(err) = get(VOID) {
+                        eprintln!("{}", err);
+                    }
+                    println!("爬VOID结束")
+                });
+            }
+            "water" => {
+                println!("getting water");
+                std::thread::spawn(|| {
+                    if let Err(err) = water() {
+                        eprintln!("{}", err);
+                    }
+                    println!("爬water结束")
+                });
+            }
+            "on" => unsafe {
+                OPEN = true;
+                println!("enabled open.");
+            }
+            "off" => unsafe {
+                OPEN = false;
+                println!("disabled open.");
+            }
+            "stop" => break,
+            _ => {}
+        }
+    }
+}
